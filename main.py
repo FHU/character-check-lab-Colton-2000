7122993a3708e9fccc59baff3ddb0b6201a96a6f
Colton-2000 <colton.french@students.fhu.edu>
2024-02-01 15:27:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+   check = word[index]
+   if check.isalpha() == True:
+       return 'letter'
+   elif check.isdigit() == True:
+       return 'number'
+   elif check.isspace() == True:
+       return 'white space'
+   else:
+       return 'unknown'
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
